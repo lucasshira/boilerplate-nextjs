ef3d699699616dfa3920e797c94e4c04abf1ade8
Lucas <lucas.shirabayashi@gmail.com>
2025-06-06 17:19:22 +0100

    Initial jest tests

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+// eslint-disable-next-line camelcase
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-  const foo = 'something'
-
   return (
     <div className={styles.page}>
       <main className={styles.main}>
